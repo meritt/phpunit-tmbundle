53a02d15e72c2ebb49104b1b2b4e79d511db633d
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2008-07-17 10:55:38 -0700

    run test should run the current test, not all tests

--- Support/runtest.rb
+++ Support/runtest.rb
@@ -4,7 +4,9 @@ require "#{ENV['TM_BUNDLE_SUPPORT']}/header.rb"
 
 html_header("Run test", "PHPUnit - Run test")
 
-phpunit_output = `cd "#{ENV['TM_PU_TEST_PATH']}"; phpunit AllTests.php`
+dir, file = File.dirname(ENV['TM_FILEPATH']), File.basename(ENV['TM_FILEPATH'])
+
+phpunit_output = `cd "#{dir}"; phpunit #{file}`
 puts phpunit_output.gsub(/[\n]/, '<br>') 
 
 html_footer()
