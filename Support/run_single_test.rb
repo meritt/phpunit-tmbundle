9c6119d8cb4572e18027a43bac430bc354aaf431
Christopher Cowan <ccowan@playpeace-lm.corp.yahoo.com>
2009-06-09 02:02:53 +0800

    Adding support for running the unit tests on a remote development server.
    Signed-off-by: Alexey Simonenko <dwarfman@gmail.com>

--- Support/run_single_test.rb
+++ Support/run_single_test.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 require "#{ENV['TM_BUNDLE_SUPPORT']}/header.rb"
 
-dir, file = ENV['TM_DIRECTORY'], ENV['TM_FILENAME']
+is_remote = ENV["REMOTE_HOST"] && ENV["REMOTE_PATH"] && ENV["LOCAL_PATH"]
+file = ENV['TM_FILENAME']
+dir = is_remote ? ENV['TM_DIRECTORY'].gsub(/#{ENV['LOCAL_PATH']}/,ENV["REMOTE_PATH"]) : ENV['TM_DIRECTORY']
 test_name = TestFinder.find_last_test_before_line(ENV['TM_FILEPATH'], ENV['TM_LINE_NUMBER'])
+cmd = is_remote ? "ssh #{ENV['REMOTE_HOST']}" : "/bin/sh"
 
 show_page("Run #{file}", "PHPUnit - Run test") do
   puts "<h1>#{test_name}</h1>"
-  output = `cd "#{dir}"; phpunit --filter #{test_name} #{file}`
+  output = `#{cmd} "cd \"#{dir}\"; phpunit --filter #{test_name} #{file}"`
   puts output.escape_html.add_code_links
 end
--- Support/run_tests.rb
+++ Support/run_tests.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 require "#{ENV['TM_BUNDLE_SUPPORT']}/header.rb"
 
-dir, file = ENV['TM_DIRECTORY'], ENV['TM_FILENAME']
+is_remote = ENV["REMOTE_HOST"] && ENV["REMOTE_PATH"] && ENV["LOCAL_PATH"]
+file = ENV['TM_FILENAME']
+dir = is_remote ? ENV['TM_DIRECTORY'].gsub(/#{ENV['LOCAL_PATH']}/,ENV["REMOTE_PATH"]) : ENV['TM_DIRECTORY']
+cmd = is_remote ? "ssh #{ENV['REMOTE_HOST']}" : "/bin/sh"
 
 show_page("Run #{file}", "PHPUnit - Run test") do
-  output = `cd "#{dir}"; phpunit #{file}`
+  output = `#{cmd} "cd \"#{dir}\"; phpunit #{file}"`
   puts output.escape_html.add_code_links
 end
