2d3ab6ce2e578235b07e989bf440aa68d2a9b851
Nikolay Bachiyski <nb+github+wordpress@nikolay.bg>
2010-06-23 18:39:05 +0800

    Add support for errors, in addition to failures

--- Support/phpunit.rb
+++ Support/phpunit.rb
@@ -12,9 +12,10 @@ module PHPUnit
 
         results[:parent][:counts] = {}
         results[:parent][:counts][:fail] = master.attributes.get_attribute('failures').value.to_i
-        results[:parent][:counts][:pass] = master.attributes.get_attribute('tests').value.to_i - results[:parent][:counts][:fail]
+        results[:parent][:counts][:error] = master.attributes.get_attribute('errors').value.to_i
+        results[:parent][:counts][:pass] = master.attributes.get_attribute('tests').value.to_i - results[:parent][:counts][:fail] - results[:parent][:counts][:error]
         results[:parent][:counts][:assertions] = master.attributes.get_attribute('tests').value.to_i
-        results[:parent][:status] = results[:parent][:counts][:fail] > 0 ? 'fail' : 'pass'
+        results[:parent][:status] = (results[:parent][:counts][:fail] +  results[:parent][:counts][:error]) > 0 ? 'fail' : 'pass'
         results[:parent][:total_time] = master.attributes.get_attribute('time').value.to_f
 
       else
@@ -31,9 +32,10 @@ module PHPUnit
 
         testsuite[:counts] = {}
         testsuite[:counts][:fail] = ts.attributes.get_attribute('failures').value.to_i
-        testsuite[:counts][:pass] = ts.attributes.get_attribute('tests').value.to_i - testsuite[:counts][:fail]
+        testsuite[:counts][:error] = ts.attributes.get_attribute('errors').value.to_i
+        testsuite[:counts][:pass] = ts.attributes.get_attribute('tests').value.to_i - testsuite[:counts][:fail] - testsuite[:counts][:error]
         testsuite[:counts][:assertions] = ts.attributes.get_attribute('tests').value.to_i
-        testsuite[:status] = testsuite[:counts][:fail] > 0 ? 'fail' : 'pass'
+        testsuite[:status] = (testsuite[:counts][:fail] + testsuite[:counts][:error]) > 0 ? 'fail' : 'pass'
         testsuite[:total_time] = ts.attributes.get_attribute('time').value.to_f
 
         ts.find("testcase").each do |tc|
@@ -42,10 +44,17 @@ module PHPUnit
           testcase[:time] = tc.attributes.get_attribute('time').value.to_f
           testcase[:assertions] = tc.attributes.get_attribute('assertions').value.to_i
           testcase[:status] = 'pass'
-          if tc.children?
-            testcase[:status] = 'fail' 
-            testcase[:message] = tc.find_first('failure/text()').content.gsub(/^\n/, '').gsub(/^\s+\w/, '').escape_html.gsub(/<br>/, '').add_code_links
-            testcase[:message] = tc.find_first('failure/text()').content.gsub(/^\n/, '').gsub(/^\s+\w/, '').gsub(/^.+#{ENV['REMOTE_PATH']}/, ENV['LOCAL_PATH']).escape_html.gsub(/<br>/, '').add_code_links if self.is_remote?
+          if tc.children?            
+            if tc.find_first('failure/text()')
+              testcase[:status] = 'fail'
+              testcase[:message] = tc.find_first('failure/text()').content.gsub(/^\n/, '').gsub(/^\s+\w/, '').escape_html.gsub(/<br>/, '').add_code_links
+              testcase[:message] = tc.find_first('failure/text()').content.gsub(/^\n/, '').gsub(/^\s+\w/, '').gsub(/^.+#{ENV['REMOTE_PATH']}/, ENV['LOCAL_PATH']).escape_html.gsub(/<br>/, '').add_code_links if self.is_remote?
+            end
+            if tc.find_first('error/text()')
+              testcase[:status] = 'error'
+              testcase[:message] = tc.find_first('error/text()').content.gsub(/^\n/, '').gsub(/^\s+\w/, '').escape_html.gsub(/<br>/, '').add_code_links
+              testcase[:message] = tc.find_first('error/text()').content.gsub(/^\n/, '').gsub(/^\s+\w/, '').gsub(/^.+#{ENV['REMOTE_PATH']}/, ENV['LOCAL_PATH']).escape_html.gsub(/<br>/, '').add_code_links if self.is_remote?
+            end  
           end
           testsuite[:cases] << testcase
         end
