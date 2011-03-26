13adf0dc3baed7d071d0ec4659af28cb2868df03
Kosmynin Aleksandr <kosmunin@gmail.com>
2011-03-26 21:36:14 +0800

    Fixing singer test run

--- Support/run_single_test.rb
+++ Support/run_single_test.rb
@@ -8,7 +8,7 @@ require "#{ENV['TM_BUNDLE_SUPPORT']}/phpunit.rb"
 test_name = TestFinder.find_last_test_before_line(ENV['TM_FILEPATH'], ENV['TM_LINE_NUMBER'])
 file = ENV['TM_FILENAME']
 dir = PHPUnit::Processor.is_remote? ? ENV['TM_DIRECTORY'].gsub(/#{ENV['LOCAL_PATH']}/,ENV["REMOTE_PATH"]) : ENV['TM_DIRECTORY']
-cmd = "cd \"#{dir}\"; phpunit --log-xml /tmp/#{file}.xml --filter #{test_name} #{file} > /dev/null; cat /tmp/#{file}.xml; rm /tmp/#{file}.xml"
+cmd = "cd \"#{dir}\"; phpunit --log-junit /tmp/#{file}.xml --filter #{test_name} #{file} > /dev/null; cat /tmp/#{file}.xml; rm /tmp/#{file}.xml"
 if PHPUnit::Processor.is_remote? 
   output = `ssh #{ENV['REMOTE_HOST']} "#{cmd}"`
 else
