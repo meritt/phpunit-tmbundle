9385fc6325f5f5e031701d1377a580e0d4e93563
Kosmynin Aleksandr <kosmunin@gmail.com>
2011-03-27 05:11:03 +0800

    Added log viewer

--- Support/run_single_test.rb
+++ Support/run_single_test.rb
@@ -8,11 +8,12 @@ require "#{ENV['TM_BUNDLE_SUPPORT']}/phpunit.rb"
 test_name = TestFinder.find_last_test_before_line(ENV['TM_FILEPATH'], ENV['TM_LINE_NUMBER'])
 file = ENV['TM_FILENAME']
 dir = PHPUnit::Processor.is_remote? ? ENV['TM_DIRECTORY'].gsub(/#{ENV['LOCAL_PATH']}/,ENV["REMOTE_PATH"]) : ENV['TM_DIRECTORY']
-cmd = "cd \"#{dir}\"; phpunit --log-junit /tmp/#{file}.xml --filter #{test_name} #{file} > /dev/null; cat /tmp/#{file}.xml; rm /tmp/#{file}.xml"
+cmd = "cd \"#{dir}\"; phpunit --log-junit /tmp/#{file}.xml --filter #{test_name} #{file} > /tmp/#{file}.log; cat /tmp/#{file}.xml; echo \"=log=\"; cat /tmp/#{file}.log; rm /tmp/#{file}.xml; rm /tmp/#{file}.log;"
 if PHPUnit::Processor.is_remote? 
   output = `ssh #{ENV['REMOTE_HOST']} "#{cmd}"`
 else
   output = `#{cmd}`
 end
-results = PHPUnit::Processor.xml(output)
+xml, log = output.split('=log=')
+results = PHPUnit::Processor.xml(xml)
 puts ERB.new(File.read("#{ENV['TM_BUNDLE_SUPPORT']}/results.html.erb")).result(binding)
--- Support/run_tests.rb
+++ Support/run_tests.rb
@@ -13,11 +13,12 @@ if infile =~ /\.xml$/
 end
 
 dir = PHPUnit::Processor.is_remote? ? ENV['TM_DIRECTORY'].gsub(/#{ENV['LOCAL_PATH']}/,ENV["REMOTE_PATH"]) : ENV['TM_DIRECTORY']
-cmd = "cd \"#{dir}\"; phpunit --log-junit /tmp/#{file}.xml #{infile} > /dev/null; if [ -f /tmp/#{file}.xml ]; then cat /tmp/#{file}.xml; rm /tmp/#{file}.xml; fi;"
+cmd = "cd \"#{dir}\"; phpunit --log-junit /tmp/#{file}.xml #{infile} > /tmp/#{file}.log; if [ -f /tmp/#{file}.xml ]; then cat /tmp/#{file}.xml; echo \"=log=\"; cat /tmp/#{file}.log; rm /tmp/#{file}.xml; rm /tmp/#{file}.log; fi;"
 if PHPUnit::Processor.is_remote? 
   output = `ssh #{ENV['REMOTE_HOST']} "#{cmd}"`
 else
   output = `#{cmd}`
 end
-results = PHPUnit::Processor.xml(output)
+xml, log = output.split('=log=')
+results = PHPUnit::Processor.xml(xml)
 puts ERB.new(File.read("#{ENV['TM_BUNDLE_SUPPORT']}/results.html.erb")).result(binding)
